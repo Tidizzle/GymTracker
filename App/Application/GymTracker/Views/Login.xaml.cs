1b9653d75f815bd6da3e9f0f9cb21df4bc2cee95
Zach Taylor <zach@planetjester.com>
2017-04-02 11:16:55 -0400

    Login Win added

--- App/Application/GymTracker/Views/Login.xaml.cs
+++ App/Application/GymTracker/Views/Login.xaml.cs
@@ -13,20 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GymTracker
+namespace GymTracker.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Page
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-            //Height = winHeight;
-            //Width = winWidth;
-            ResizeMode = ResizeMode.NoResize;
-            MainFrame.Content = new Views.Login();
         }
     }
 }
