2640e254f5fe75cef82f144dcbf59e626bc612f9
Zach Taylor <zach@planetjester.com>
2017-04-03 17:52:54 -0400

    db

---
+++ App/Application/GymTracker/Classes/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTracker.Classes
+{
+    public static class AppState
+    {
+        public const string DbCon = "sqlite"
+    }
+}
