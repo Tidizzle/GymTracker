fc39ec51b3f52429326a4cfce0f33910c4d3ca4d
Zach Taylor <zach@planetjester.com>
2017-04-07 21:50:32 -0400

    login added
    Back from being sick bitchessss

--- App/Application/GymTracker/Views/LoginToAccount.xaml.cs
+++ App/Application/GymTracker/Views/LoginToAccount.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Extensions.Debug;
+using System.Linq;
+using GymTracker.Classes;
 
 namespace GymTracker.Views
 {
@@ -27,11 +30,33 @@ namespace GymTracker.Views
         {
             InitializeComponent();
             ParentWin = parent;
+            LoginButton.IsEnabled = false;
         }
 
         private void LoginButton_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            var query = from user in AppState.Userlist
+                where user.Username == UsernameBox.Text && user.Password == PasswordBox.Password
+                select user;
+
+            var i = 0;
+            foreach (var user in query)
+            {
+                i++;
+            }
+
+            if (i > 0)
+            {
+                //Login fuction
+
+                MessageBox.Show("Login Accepted");
+            }
+            else
+            {
+                PasswordBox.Clear();
+                UsernameBox.Clear();
+                MessageBox.Show("Username or Password Incorrect", "Login Error", MessageBoxButton.OK);
+            }
         }
 
         private void CreateAccount_OnClick(object sender, RoutedEventArgs e)
@@ -43,6 +68,43 @@ namespace GymTracker.Views
         private void ResetPassword_OnClick(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private bool UNEntered = false;
+        private bool PassEntered = false;
+
+        private void UsernameBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(UsernameBox.Text))
+            {
+                UNEntered = true;
+                if (PassEntered == true)
+                {
+                    LoginButton.IsEnabled = true;
+                }
+            }
+            else
+            {
+                LoginButton.IsEnabled = false;
+            }
+            
+           
+        }
+
+        private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(PasswordBox.Password))
+            {
+                PassEntered = true;
+                if (UNEntered == true)
+                {
+                    LoginButton.IsEnabled = true;
+                }
+            }
+            else
+            {
+                LoginButton.IsEnabled = false;
+            }
         }
     }
 }
