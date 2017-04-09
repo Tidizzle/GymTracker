24fcfb40b647a64f4abf04cf6e164f659eb274c2
Zach Taylor <zach@planetjester.com>
2017-04-08 21:41:49 -0400

    find account

--- App/Application/GymTracker/Views/ResetPassword.xaml.cs
+++ App/Application/GymTracker/Views/ResetPassword.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GymTracker.Views.SubViews;
 
 namespace GymTracker.Views
 {
@@ -20,9 +21,38 @@ namespace GymTracker.Views
     /// </summary>
     public partial class ResetPassword : Page
     {
+        private ResetFindAcct FindAcctInstnc;
+        private frameMode ButtonMode;
+        private enum frameMode { Find, Reset}
+
+        private Login ParentWin;
+
         public ResetPassword(Login parent)
         {
             InitializeComponent();
+            ParentWin = parent;
+            FindAcctInstnc = new ResetFindAcct(this);
+
+            ResetFrame.Content = FindAcctInstnc;
+            FrameButton.Content = "Find Account";
+            ButtonMode = frameMode.Find;
+        }
+
+        private void FrameButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ButtonMode == frameMode.Find)
+            {
+
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParentWin.LoginFrame.Content = ParentWin.LoginInstnc;
         }
     }
 }
